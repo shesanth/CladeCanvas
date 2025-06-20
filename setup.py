5dcccea71ca1508af62c7306ac2d0de2e1d08b7c
shesanth <shesanth.r@gmail.com>
2025-06-20 01:20:44 -0700

    fix CI

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="cladecanvas",
     version="0.1.0",
-    package_dir={"": "cladecanvas"},
-    packages=find_packages(where="cladecanvas"),
+    packages=find_packages(),
 )
